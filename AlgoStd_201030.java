4080d30102f7c8400ff170e35d9258d8c38d07a1
wow555wow <60210158+wow555wow@users.noreply.github.com>
2020-10-30 21:02:48 +0900

    미완) [3차]N진수 게임
    N진수 게임
    튜브가 활동하는 코딩 동아리에서는 전통적으로 해오는 게임이 있다. 이 게임은 여러 사람이 둥글게 앉아서 숫자를 하나씩 차례대로 말하는 게임인데, 규칙은 다음과 같다.
    
    숫자를 0부터 시작해서 차례대로 말한다. 첫 번째 사람은 0, 두 번째 사람은 1, … 열 번째 사람은 9를 말한다.
    10 이상의 숫자부터는 한 자리씩 끊어서 말한다. 즉 열한 번째 사람은 10의 첫 자리인 1, 열두 번째 사람은 둘째 자리인 0을 말한다.
    이렇게 게임을 진행할 경우,
    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1, 1, 2, 1, 3, 1, 4, …
    순으로 숫자를 말하면 된다.
    
    한편 코딩 동아리 일원들은 컴퓨터를 다루는 사람답게 이진수로 이 게임을 진행하기도 하는데, 이 경우에는
    0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, …
    순으로 숫자를 말하면 된다.
    
    이진수로 진행하는 게임에 익숙해져 질려가던 사람들은 좀 더 난이도를 높이기 위해 이진법에서 십육진법까지 모든 진법으로 게임을 진행해보기로 했다. 숫자 게임이 익숙하지 않은 튜브는 게임에 져서 벌칙을 받는 굴욕을 피하기 위해, 자신이 말해야 하는 숫자를 스마트폰에 미리 출력해주는 프로그램을 만들려고 한다. 튜브의 프로그램을 구현하라.
    
    입력 형식
    진법 n, 미리 구할 숫자의 갯수 t, 게임에 참가하는 인원 m, 튜브의 순서 p 가 주어진다.
    
    2 ≦ n ≦ 16
    0 ＜ t ≦ 1000
    2 ≦ m ≦ 100
    1 ≦ p ≦ m
    출력 형식
    튜브가 말해야 하는 숫자 t개를 공백 없이 차례대로 나타낸 문자열. 단, 10~15는 각각 대문자 A~F로 출력한다.
    
    입출력 예제
    n	t	m	p	result
    2	4	2	1	0111
    16	16	2	1	02468ACE11111111
    16	16	2	2	13579BDF01234567

---
+++ AlgoStd_201030.java
@@ -0,0 +1,120 @@
+package level2;
+
+public class AlgoStd_201030 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+//		solution_2(2, 4, 2, 1);
+		solution_2(16, 16, 2, 1);
+
+	}
+	
+	
+	/* 진법변환 참조
+	 * 10진법 -> n진법 변환 : String.toBinaryString 등
+	 * n진법 -> 10진법 변환 : Integer.parseInt(i, n);
+	 */
+	
+	
+	/** [3차] n진수 게임 : n진법 숫자를 차례로 말한다. 자릿수로 끊어서 말함
+	 * 
+	 * elaps : 22:30 ~ 
+	 * 
+	 * @param n : 진법
+	 * @param t : 미리 구할 숫자의 갯수(튜브가 말할)
+	 * @param m : 게임에 참가하는 인원
+	 * @param p : 튜브의 순서
+	 * @return : 튜브가 말해야 하는 숫자를 공백 없이 차례로 나타낸 문자열 (10~15는 A~F로 표현)
+	 */
+	public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        
+        //숫자 증가(제한 : 미리 구할 숫자의 갯수)
+        int order=0;
+        for(int i=0; i<(m*t); i++) {	// 참가인원(튜브포함) * round 수
+        	
+        	//진법변환 : n으로 나누어서 그 나머지를 역순으로 나열한다.
+        	int decimal = i;
+        	String converted = "";
+        	while(decimal>0) {
+        		
+        		// 두자리 숫자 처리
+        		int rest = decimal%n;
+        		if(10 <= rest) {
+        			switch(rest) {
+    				case(10) : rest = 'A';
+    				case(11) : rest = 'B';
+    				case(12) : rest = 'C';
+    				case(13) : rest = 'D';
+    				case(14) : rest = 'E';
+        			}
+        		}
+        		
+        		converted = rest + converted;
+        		decimal /= n;	//1 이후 0까지 도달 후 반복문 탈출
+        	}
+        	
+        	for(int j=0; j<converted.length(); j++) {	//진법변환 한 수의 자릿수만큼 반복
+        		if(order%m == p-1) {	// 참가인원들의 순번 중 튜브의 순번과 같은 경우
+        			answer += converted.charAt(j);
+        		}
+        		order++;	//순서 증가
+        	}
+        	
+        }
+        
+        return answer;
+    }
+	
+	
+
+	public static String solution_2(int n, int t, int m, int p) {
+        String answer = "";
+        
+        int i=0;	//숫자 증가용
+        int order=1;	//말할 순번 증가용
+        while(t > answer.length()) {	// t개 구할 떄까지 반복
+        	
+        	//n진수 구하기
+        	int decimal = i++;	//decimal(진수변환용 변수)에 넣은 뒤 i 증가
+        	String converted = "";
+        	while(0 < decimal) {
+        		
+        		// 두자리 숫자 처리
+        		int rest = decimal%n;
+        		if(10 >= rest) {
+        			switch(rest) {
+    				case(10) : rest = 'A';
+    				case(11) : rest = 'B';
+    				case(12) : rest = 'C';
+    				case(13) : rest = 'D';
+    				case(14) : rest = 'E';
+        			}
+        		}
+        		
+        		converted = rest + converted;
+        		decimal /= n;	//1 이후 0까지 도달 후 반복문 탈출
+        	}
+        	if(0==converted.length()) converted = "0";	//0인경우 while로 진입하지 않으므로 초기값 0 필요	
+        	System.out.println("진수변환 완료 :: " + converted);
+        	
+        	
+        	//구한 n진수를 하나씩 나열하며 튜브 순서 찾기
+        	for(int j=0; j<converted.length(); j++) {
+        		System.out.println("order[" + order + "] people[" + m + "] tube_order[" + (p) + "]");
+        		if(order%m == p) {	// 참가인원들의 순번 중 튜브의 순번과 같은 경우
+        			answer += converted.charAt(j);
+        			System.out.println("answer >>> " + answer);
+        			
+        			if(t == answer.length()) break;
+        		}
+        		order++;	//순서 증가
+        	}
+        	
+        }
+        
+        return answer;
+	}
+}
