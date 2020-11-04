016df59efc333cb192289f09e0d51a0febab4b2a
wow555wow <60210158+wow555wow@users.noreply.github.com>
2020-11-04 23:35:57 +0900

    다리를 지나는 트럭 주석 추가
    < 생각의 흐름 >
    1. 트럭 수만큼의 배열을 만들어서 다리를 이동시키려고 함(idx로 다리의 시작과 끝을 이동시키기)
    2. 서로 다른 트럭이 다리에 오르고 내리는 것이 동시에 가능하다는 것을 깨달음.. 
       다리에 최대로 올릴 수 있는 트럭을 모두 올리고 그 수를 계산하려고 했는데(다리길이만큼 한번에 시간 더하려고) > 한대씩 넣고 빼면서 무게연산이 필요하다는것을 깨달음
       한번에 건널 수 있는 트럭들이 다 건넜을 때 한꺼번에 초기화 하려고 함 (다리가 1차선이라 한대씩밖에 못지나가는데 마치 100차선인것처럼 생각했음)
    3. 최대하중과 함께 다리 길이를 고려해야 한다는 것을 깨달음 (트럭이 아무리 가벼워도 길이가 모자라면 다리 위에 넣을 수 없음)
        트럭을 한번에 올려놓고 다리 길이만큼 시간을 증가시키려고 했는데 하다보니 이상한 것 같아서 시간증가를 어떻게 시켜야 할지 고민함
        다리 위에 있는 트럭들을 한번에 넣고 꺼내면 안되고 한대씩 넣고 꺼내야 한다는 것을 깨달음
    4. 다리 위에 있는 트럭의 수에 대한 집착을 버림 (시간을 한번에 더해주려고 다리 위 트럭의 수를 구한건데 이래선 안된다는걸 깨달음)
        반복시마다 시간을 더하기로 함
        다리의 끝에 도달하면 트럭을 무조건 빼야 함
        queueIdx를 어떻게 제어해야할지 어쩔 줄 몰라함 (어디가 다리 시작인지? 다리 사이즈는 어떻게 해야할지?
    5. 트럭이 올라가기만 하는 구간 / 오르고 내리는 구간 / 내리기만 하는 구간 으로 나누어 생각하기로 함
        여기에 무게체크도 같이
        트럭을 내리는 경우의 queueIdx 처리를 어떻게 해야할지 아직도 고민함
        마지막 트럭을 넣은 뒤 걸리는 시간을 +다리길이 해서 구하기로 함 (마지막 트럭이 다리에 올라가는 순간 1초 시작 > 다리길이만큼 시간이 지나면 마지막 트럭이 내림)
    6. queueIdx 처리할필요가 없다는걸 꺠달음 : 큐가 전부 다 차고 나서부터(다리길이만큼 시간이 지난 후부터) 트럭을 빼기 시작할 것이므로 굳이 인덱스 처리할 필요가 없음
         내릴 트럭의 자리에 새로 올라올 트럭을 집어 넣을 것이기 때문에 (덮어쓰기 할것임)
    7. 넣기만할때를 예외처리할게아니라 빼는경우를 예외처리하는게 코드를 더 적게쓴다 (트럭을 넣는 과정은 1. 넣기만 하는 경우, 2. 버리기만 하는 경우 에서 모두 수행하므로)
        넣기만할때를 예외처리할땐 안헷갈렸는데 조건을 뒤집으니 헷갈리기 시작함..

--- AlgoStd_201103.java
+++ AlgoStd_201103.java
@@ -1,44 +1,85 @@
 import java.util.*;
 class Solution {
+    
+    /** 201103 다리를 지나는 트럭
+    *
+    * 동시에 2트럭이 지나갈 수는 없고, 1초 뒤에 지나갈 수 있음
+    * 트럭에게는 순서가 있음. 순서대로 다리를 지나야 함
+    *
+    * 1. 트럭 배열을 돌면서
+    * 2. 다리 하중과 트럭 무게를 비교
+    *  2-1. 다리 하중과 같으면 지금 트럭만
+    *  2-2. 다리 하중보다 작으면 >> 지금트럭+다음트럭 무게와 최대하중 비교
+    *   2-2-1. 다리하중보다 작으면 >> 지금트럭+다음트럭+다다음트럭 무게와 최대하중 비교 (반복)
+    *  최대하중에 가까워질 때까지 트럭을 다리로 보내고 (다리길이도 생각해)
+    *
+    * 무게체크 시점
+    *  1. 트럭이 다리에 진입할 때
+    *  2. 1초가 지나 다음 트럭이 다리에 진입할 때
+    *  3. 트럭이 다리에서 나갈 때
+    *
+    * Queue > 인터페이스만 정의되어 있어서 선택해서 새로 구현해 줘야 함.
+    *  priorityQueue(우선순위큐), deque(앞뒤 모두에서 꺼낼 수 있음 : stack과 queue로 모두 사용 가능)
+    *
+    * @param bridge_length
+    * @param weight
+    * @param truck_weigth
+    * @return 모든 트럭이 다리를 건너는데 걸리는 최단시간
+    */
+    
     public int solution(int bridge_length, int weight, int[] truck_weight) {
-        int answer = 0;
+        int answer = 0; //소요시간
         
-        int weight_sum = 0;
-        int[] circleQueue = new int[bridge_length];
-        int queueIdx = 0;
+        int weight_sum = 0; //다리 위에 올라간 트럭들의 무게 합
+        int[] circleQueue = new int[bridge_length]; // 다리 위(트럭을 올릴 공간, 다리 길이만큼 정의), 원형큐 사용하여 길이 고정함.
+        int queueIdx = 0;   //트럭을 넣을 큐의 인덱스
         
-        for(int i=0; i<truck_weight.length; ){
-            int nextTruck = 0;
-            if(++answer < bridge_length) {
-                if(weight >= weight_sum + truck_weight[i]) {
-                nextTruck = truck_weight[i++];
-            }
-            circleQueue[queueIdx++] = nextTruck;
-            queueIdx %= bridge_length;
-            weight_sum += nextTruck;
-                
-                /*
-                if(weight >= weight_sum + truck_weight[i]) {
-                    circleQueue[queueIdx++] = truck_weight[i];
-                    queueIdx %= bridge_length;
-                    weight_sum += truck_weight[i++];
+        /*
+        * 1. 큐가 다 차기 전까지는 넣는다 (근데 무게체크도 해야 함)
+        * 2. 큐가 다 차면 버리면서 넣는다 (근데 무게체크도 해야 함)
+        * 3. 더 넣을 트럭이 없으면 버리기만 한다
+        *
+        */
+        
+        for(int i=0; i<truck_weight.length; ){  //트럭이 모두 다리를 지날 때까지 반복
+            
+            /*
+            int nextTruck = 0;  //다음에 다리를 지날 트럭의 무게, 최대하중에 다다라서 다음에 트럭이 올 수 없는 경우는 0으로 표시한다 (무게가 없음 0)
+            if(++answer < bridge_length) {  // 반복시마다 시간증가, 시간이 다리길이보다 적게 지난 때에는 다리에 트럭을 넣기만 할 수 있음 (1. 큐가 다 차기 전까지는 넣기만 한다)
+                if(weight >= weight_sum + truck_weight[i]) {    // 무게체크 : 다리의 최대하중 >= 현재 다리 위 트럭의 무게합 + 다음에 올 트럭의 무게
+                    nextTruck = truck_weight[i++];  // 다리 최대하중보다 작으면 다음에 올 트럭의 무게를 더해줌
                 }
-                */
-                continue;
+            circleQueue[queueIdx++] = nextTruck;    // 큐의 현재 위치에 올라올 트럭을 넣고, 인덱스 증가(다음에 바로 넣을 수 있도록 준비)
+            queueIdx %= bridge_length;  // 원형큐 인덱스처리. 큐의 길이로 나눈 나머지를 인덱스로 사용.
+            weight_sum += nextTruck;    // 다리 위 트럭 무게 총합에 올라올 트럭의 무게를 더해줌
+               
+            continue;
+            }
+            */
+            
+            // 2. 큐가 다 차면 버리면서 넣는다 : 다리길이가 2일 때 2초가 지난 상황. 근데 왜 먼저 빼지? 3초일때 빠지는거 아닌가? 큐인덱스랑 잘 엮어서 생각해봐
+            // 시간이 2흘렀을때 큐인덱스는 1. 들어있는게 없어서 뺄 것도 없기 때문에 등호가 있는 경우에도 상관이 없었던 듯. 등호가 빠지는게 맞는 조건인 것 같다.
+            // 트럭이 다리길이에 다다랐을 때 > 트럭의 빼기와 올리기가 동시에 일어난다. 다음에 올려주려면 빼는걸 먼저 해줘야 함. (이부분 헷갈리는데 다시 생각해보자)
+            //if(++answer >= bridge_length) { // 반복시마다 시간증가(1초에서 시작, 다리에 올리는 순간 1초가 지나므로), 시간이 다리길이만큼 지난 뒤에는 트럭을 다리 위에서 꺼낼 수 있다.
+            
+            // 2. 큐가 다 차면 버리면서 넣는다
+            if(++answer > bridge_length) {  // 반복시마다 시간증가(1초에서 시작, 다리에 올리는 순간 1초가 지나므로), 시간이 다리길이만큼 지난 뒤에는 트럭을 다리 위에서 꺼낼 수 있다.
+                //다리를 다 지나면 트럭은 무조건 내려야만 함.
+                weight_sum -= circleQueue[queueIdx];    // 다리 위 트럭 무게 총합에서 현재 위치(다음에 트럭 넣을 자리 == 다리에 끝에 다다라서 내려야 할 트럭이 있는 자리)의 트럭을 꺼낸다
+                // 원형큐라 뺄 트럭의 인덱스에 다음 트럭을 올릴 것이므로 따로 배열에서 빼는 연산은 수행하지 않음
             }
             
-            weight_sum -= circleQueue[queueIdx];
-            
-            //int nextTruck = 0;
-            if(weight >= weight_sum + truck_weight[i]) {
-                nextTruck = truck_weight[i++];
+            // 다리 위에 트럭 올리기
+            int nextTruck = 0;  //다음에 다리를 지날 트럭의 무게, 최대하중에 다다라서 다음에 트럭이 올 수 없는 경우는 0으로 표시한다 (무게는 없지만 빈공간으로 자리는 차지해야 함)
+            if(weight >= weight_sum + truck_weight[i]) {    // 무게체크 : 다리의 최대하중 >= 현재 다리 위 트럭의 무게합 + 다음에 올 트럭의 무게
+                nextTruck = truck_weight[i++];  // 다리 최대하중보다 작으면 다음에 올 트럭의 무게를 더해줌
             }
-            circleQueue[queueIdx++] = nextTruck;
-            queueIdx %= bridge_length;
-            weight_sum += nextTruck;
-        }
+            circleQueue[queueIdx++] = nextTruck;    // 큐의 현재 위치에 올라올 트럭을 넣고, 인덱스 증가(다음에 바로 넣을 수 있도록 준비)
+            queueIdx %= bridge_length;  // 원형큐 인덱스처리. 큐의 길이로 나눈 나머지를 인덱스로 사용.
+            weight_sum += nextTruck;    // 다리 위 트럭 무게 총합에 올라올 트럭의 무게를 더해줌
+        }   // 마지막 트럭이 다리에 올라가는 순간 반복 종료
         
-        answer += bridge_length;
+        answer += bridge_length;    // 이후 마지막 트럭이 다리를 다 지나기만 하면 되므로 > 다리길이만큼의 시간이 지나면 모든 트럭이 다리를 지나게 됨
         
         
         return answer;
