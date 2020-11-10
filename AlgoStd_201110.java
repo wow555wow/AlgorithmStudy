6949e2d4cefab5c2a683cca080f4f920703c2e79
wow555wow <60210158+wow555wow@users.noreply.github.com>
2020-11-10 17:44:49 +0900

    Update AlgoStd_201110.java

--- AlgoStd_201110.java
+++ AlgoStd_201110.java
@@ -3,19 +3,20 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         
+        //정렬해서 가장 무거운 사람과 가장 가벼운 사람을 짝짓기
         Arrays.sort(people);
-        int start=0;
-        int end=people.length-1;
+        int start=0; //가장 가벼운 사람
+        int end=people.length-1; //가장 무거운 사람
         
         while(start<end) {
-            if(limit - people[end] < 40 || limit - people[end] - people[start] < 0) {
+            if(limit - people[end] < 40 || limit - people[end] - people[start] < 0) { // 한명밖에 태울 수 없음
                 end--;
                 answer++;
-            }else if(limit - people[end] - people[start] >= 0) {
+            }else if(limit - people[end] - people[start] >= 0) { // 두명 태울 수 있음(두명이 최대)
                 end--;
                 start++;
                 answer++;
-            }
+            } // 위 두 조건으로 모든 케이스 커버
             
             if(start==end) { //마지막 한명만 남은 경우
                 answer++;
