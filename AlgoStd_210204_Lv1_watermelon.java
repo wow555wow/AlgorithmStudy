4374e4abf930f67de7cbc28b9ad6df947b41bfcd
wow555wow <60210158+wow555wow@users.noreply.github.com>
2021-02-05 21:31:32 +0900

    Lv1. 수박수박수
    고정문자열은 보통 대문자로 쓴다.
    일반 변수와 쉽게 구별하기 위함인 듯.
    고정문자열 변수명을 [타입_변수명 ] 형태로 쓰는 경우도 있음.

--- AlgoStd_210204_Lv1_watermelon.java
+++ AlgoStd_210204_Lv1_watermelon.java
@@ -1,15 +1,15 @@
 class Solution {
     
-    String even = "수";
-    String odd = "박";
+    String WM_EVEN = "수";
+    String WM_ODD = "박";
     
     public String solution(int n) {
         String answer = "";
         
         for(int i=0; i<n; i++) {
             if(0 == i%2)
-                answer += even;
-            else answer += odd;
+                answer += WM_EVEN;
+            else answer += WM_ODD;
         }
         
         return answer;
