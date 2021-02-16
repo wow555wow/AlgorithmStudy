cf51268cd2eff0f0cb5b94e0acc3a697b5542ed6
wow555wow <60210158+wow555wow@users.noreply.github.com>
2021-02-16 21:59:09 +0900

    Lv2 올바른 괄호 > Stack이 비어 있을 때 peek() 하면 런타임 에러 발생
    Stack이 비어 있을 때 peek() 하면 런타임 에러 발생

--- AlgoStd_210216_Lv2_CollectBracket.java
+++ AlgoStd_210216_Lv2_CollectBracket.java
@@ -4,13 +4,13 @@ class Solution {
         boolean answer = true;
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        //System.out.println("Hello Java");
         
         Stack<Character> stack = new Stack<Character>();
         stack.push(s.charAt(0));
         
         for(int i=1; i<s.length(); i++) {
-            if(s.charAt(i) == ')' && stack.peek() == '(') {
+            if(s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(') { //스택이 비어있는데 peek() 하면 런타임 에러 발생하는 듯.
             	stack.pop();
             }else {
             	stack.push(s.charAt(i));
